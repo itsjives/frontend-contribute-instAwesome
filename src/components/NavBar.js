1ce1d9b8738bed426849c0f379986f0b76efab7c
Johnny Capra <johnnycapra@Johnnys-MacBook-Pro.local>
2018-02-04 11:19:53 -0700

    added interval to switch between different testimonials, added collapse nav-bar, fixed some styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ class NavBar extends React.Component {
   render () {
     return (
       <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo">Logo</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <div className="nav-wrapper">
+          <a href="#" id="nav-mobile" className="brand-logo left button-collapse">Logo</a>
+          <ul id="nav-mobile" className="right button-collapse">
             <li><a href="sass.html">Process</a></li>
             <li><a href="badges.html">Ventures</a></li>
             <li><a href="collapsible.html">Community</a></li>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 import '../styles/Testimonials.css'
 
+
 class Testimonials extends Component {
 
+
+  state = {
+    counter: 0,
+    currentT: []
+  }
+
+  text = ["Welcome", "Hi", "Sup dude",
+  `this is a really long testimony about cool stuff. Work with us and we will make
+   you the most successful version of yourself there could be.
+   Taco bell is kinda sweet sometimes. One time I ate a jalapeno.`
+  ];
+  
+  currentT = [];
+  counter = 0;
+
+  change = () => {
+    this.currentT = this.text[this.counter];
+    console.log(this.currentT);
+    this.setState({
+      counter: this.counter,
+      currentT: this.currentT
+    })
+    this.counter++;
+    if (this.counter >= this.text.length) {
+      this.counter = 0;
+    }
+  }
+
+
   render () {
+
+    console.log(this.state);
+
+    setTimeout(this.change, 5000)
+
     return (
       <div className='testimony'>
         <div className='currentT'>
-          <p>
-            As a small nonprofit, working with Conscious was our first time ever contracting with an outside company to
-            assist with our communications strategy, development, and planning. We really appreciated their patience and willingness to adapt
-            to the unique needs of our organization, and we are very happy with the progress that we've made with the Conscious team!
-          </p>
+          <p>{this.state.currentT}</p>
         </div>
       </div>
     )
